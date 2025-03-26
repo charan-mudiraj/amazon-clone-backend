ed6b77c6ce5d4f213546b407fb7f2bc8d1547e8f
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2025-03-27 03:28:04 +0530

    accept post req

--- api/views.py
+++ api/views.py
@@ -2,10 +2,19 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework import status
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])  # Allow both GET and POST
 def get_products(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
