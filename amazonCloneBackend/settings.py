ba42d4a6fb8a2a606c0519e4d7a5eac63bff819f
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2025-03-28 01:06:27 +0530

    cors

--- amazonCloneBackend/settings.py
+++ amazonCloneBackend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,9 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # ✅ Add this
 ]
 
 ROOT_URLCONF = 'amazonCloneBackend.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True  # ✅ Allows requests from any frontend
 
 TEMPLATES = [
     {
